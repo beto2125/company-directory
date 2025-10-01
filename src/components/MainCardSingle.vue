e424f15a6303142656708c4b08624a41ff4266ad
beto2125 <betobeto60@gmail.com>
2025-10-01 09:23:52 -0500

    change faker position

--- src/components/MainCardSingle.vue
+++ src/components/MainCardSingle.vue
@@ -3,26 +3,24 @@ import { faker } from '@faker-js/faker'
 
 const firstname = faker.person.firstName()
 const lastname = faker.person.lastName()
-const fullname = faker.person.fullName({firstName: firstname, lastName: lastname})
+const fullname = faker.person.fullName({ firstName: firstname, lastName: lastname })
 const job_title = faker.person.jobTitle()
 const bio = faker.lorem.lines(2)
 const username = faker.internet.userName({ firstName: firstname, lastName: lastname }).toLowerCase()
-const imageUrl = faker.image.urlLoremFlickr({category: 'cat', height:480, width: 960})
+
+// correct order: width, height, category
+const imageUrl = faker.image.urlLoremFlickr({ width: 960, height: 480, category: 'cat' })
 </script>
 
-
 <template>
-
-    <!-- {{ fullname }} <br /> -->
-    <RouterLink :to="{name: 'CardDetails', params: {id:username}}">
+  <RouterLink :to="{name: 'CardDetails', params: {id: username}}">
     <div class="border border-gray-400 rounded-lg shadow bg-gray-200 h-full">
-        <img :src="faker.image.urlLoremFlickr({category:'cat'})" class ="rounded-t-xl"/>
-        <div class="p-4">
-            <p class="mb-2 text-2xl font-bold">{{ fullname }}</p>
-            <p class="mb-3 font-normal text-gray-700">{{ job_title }}</p>
-            <p class="mb-3 font-normal text-gray-700">{{ bio }}</p>
-        </div>
+      <img :src="imageUrl" class="rounded-t-xl"/>
+      <div class="p-4">
+        <p class="mb-2 text-2xl font-bold">{{ fullname }}</p>
+        <p class="mb-3 font-normal text-gray-700">{{ job_title }}</p>
+        <p class="mb-3 font-normal text-gray-700">{{ bio }}</p>
+      </div>
     </div>
-    </RouterLink>
-    
-</template> 
+  </RouterLink>
+</template>
